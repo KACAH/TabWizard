244de9f64beefa68b880f2d3ceae605d041ad769
Daniils <Jurval@val-4d76331103c.(none)>
2012-04-04 11:13:48 +0300

    added absolute random music generator

--- src/gui/SomeSuggestionFrame.java
+++ src/gui/SomeSuggestionFrame.java
@@ -1,20 +1,40 @@
 package gui;
 
+import java.awt.Font;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
+import javax.swing.DefaultComboBoxModel;
 import javax.swing.ImageIcon;
+import javax.swing.JComboBox;
 import javax.swing.JComponent;
+import javax.swing.JFormattedTextField;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JSpinner;
 import javax.swing.JTabbedPane;
+import javax.swing.SpinnerNumberModel;
+
+import music.TWGenerate;
+import music.TWScaleManager;
+import datastruct.TWDataException;
 
 
 public class SomeSuggestionFrame extends JFrame{
 
 	private static final long serialVersionUID = -2305852043390443274L;
+	
+	private JComponent panelTempo;
+	private JComponent panelScale;
+	private JComponent panelGenerate;
+	private JSpinner Tempo;
+	private JComboBox<String> scales;
 
 	public SomeSuggestionFrame()
 	{
@@ -29,32 +49,90 @@ public class SomeSuggestionFrame extends JFrame{
 	    JTabbedPane tabbedPane = new JTabbedPane();
 	    ImageIcon icon = new ImageIcon("icon.png");
 
-	    JComponent panel1 = makeTextPanel("Song tracks");
-	    tabbedPane.addTab("Tab 1", icon, panel1);
+	    panelTempo = makeTextPanel("Song tempo");
+	    setTempoPanel();
+		tabbedPane.addTab("Tempo", icon, panelTempo);
 
-	    JComponent panel2 = makeTextPanel("Panel #2");
-	    tabbedPane.addTab("Tab 2", icon, panel2);
+		panelScale = makeTextPanel("Set Scale");
+	    setScalePanel();
+	    tabbedPane.addTab("Scale", icon, panelScale);
 
 	    JComponent panel3 = makeTextPanel("Panel #3");
 	    tabbedPane.addTab("Tab 3", icon, panel3);
 
-	    JComponent panel4 = makeTextPanel("Panel #4");
-	    tabbedPane.addTab("Tab 4", icon, panel4);
+	    panelGenerate = makeTextPanel("Generate");
+	    setGeneratePanel();
+	    tabbedPane.addTab("Generate song", icon, panelGenerate);
 	    
 		return tabbedPane;
+	}
+	
+	private void setTempoPanel()
+	{
+		Tempo = new JSpinner(new SpinnerNumberModel(150, 1, 300, 1));
+		JFormattedTextField tf = ((JSpinner.DefaultEditor)Tempo.getEditor()).getTextField();
+		tf.setEditable(false);
+		Tempo.setFont( new Font("Dialog", Font.PLAIN, 35));
+		
+		panelTempo.add(Tempo);
+	}
+	
+	private void setScalePanel()
+	{
+		scales  = new JComboBox<String>();
+		DefaultComboBoxModel<String> ñomboModel = new DefaultComboBoxModel<String>();
+		scales.setModel(ñomboModel);
+		scales.setFont( new Font("Dialog", Font.PLAIN, 25));
+		
+		for(int i = 0; i < TWScaleManager.StringScales.size(); i++)
+		{
+			String[] ScaleName = TWScaleManager.StringScales.get(i).split(" ");
+			scales.addItem(ScaleName[0]);
+		}
+		panelScale.add(scales);
+	}
+	
+	private void setGeneratePanel()
+	{
+		RoundButton generate = new RoundButton();
+		ImageIcon cup = new ImageIcon("media//Button.png");
+		generate.setIcon(cup);
+		generate.setToolTipText("Generate new music fragment");
+		
+		generate.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				try {
+					
+					TWGenerate.Generate((Integer)Tempo.getValue(), scales.getSelectedItem().toString());
+					
+				} catch (TWDataException e1) {
+					e1.printStackTrace();
+				} catch (FileNotFoundException e2) {
+					e2.printStackTrace();
+				} catch (IOException e3) {
+					e3.printStackTrace();
+				}
+			}
+		});
+		
+		panelGenerate.add(generate);
 	}
 	
 	protected JComponent makeTextPanel(String text) {
 	    JPanel panel = new JPanel(false);
 	    JLabel filler = new JLabel(text);
+	    filler.setFont(new Font("Dialog", Font.PLAIN, 35));
 	    filler.setHorizontalAlignment(JLabel.CENTER);
-	    panel.setLayout(new GridLayout(1, 1));
+	    panel.setLayout(new GridLayout(2, 0));
 	    panel.add(filler);
 	    return panel;
 	  }
 	
 	private void init()
 	{
+		TWScaleManager.loadScales("data//Scales.twd");
+		
 		this.setResizable(false);
 		
 		this.addWindowListener(new WindowAdapter()
